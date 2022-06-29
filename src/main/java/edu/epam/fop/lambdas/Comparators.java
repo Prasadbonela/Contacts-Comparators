a69f3ae99009b081d5b03a2275369c2487e52cf6
Vadim Markov <vadim_markov@epam.com>
2022-06-30 01:15:06 +0200

    Initial

---
+++ src/main/java/edu/epam/fop/lambdas/Comparators.java
@@ -0,0 +1,25 @@
+package edu.epam.fop.lambdas;
+
+import java.util.Comparator;
+
+public interface Comparators {
+
+  // TODO write you code here
+  static Comparator<Address> ZIP_CODE_COMPARATOR = null;
+
+  static Comparator<Address> STREET_COMPARATOR = null;
+
+  static Comparator<Address> REGION_COMPARATOR = null;
+
+  static Comparator<Address> ADDRESS_COMPARATOR = null;
+
+  static Comparator<Person> FULL_NAME_COMPARATOR = null;
+
+  static Comparator<Person> BIRTHDATE_COMPARATOR = null;
+
+  static Comparator<Person> PERSON_COMPARATOR = null;
+
+  static Comparator<Company> REGISTRATION_ID_COMPARATOR = null;
+
+  static Comparator<Company> COMPANY_COMPARATOR = null;
+}
